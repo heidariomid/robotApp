ec5756f5a9d4cc5733907899835725c1d490ce1a
Omid Heidari <omid_heidari90@yahoo.com>
2020-09-25 15:28:18 +0330

    Updates

---
+++ precache-manifest.97296d61c1d8988f84ef4419165c7c44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecc2f305810349f9ab027227396401b",
+    "url": "/robotApp/index.html"
+  },
+  {
+    "revision": "b13ce4c96b98114038f2",
+    "url": "/robotApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ddf5deba7e801d5a1941",
+    "url": "/robotApp/static/css/main.1b4593dd.chunk.css"
+  },
+  {
+    "revision": "b13ce4c96b98114038f2",
+    "url": "/robotApp/static/js/2.48cf9d23.chunk.js"
+  },
+  {
+    "revision": "b42f36b5d572c611b888796d07d4a679",
+    "url": "/robotApp/static/js/2.48cf9d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf5deba7e801d5a1941",
+    "url": "/robotApp/static/js/main.abc744ae.chunk.js"
+  },
+  {
+    "revision": "fe44aea8c778a604af03",
+    "url": "/robotApp/static/js/runtime-main.84dee4ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
